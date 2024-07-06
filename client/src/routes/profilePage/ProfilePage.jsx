c33552ca957d7db43c964b274ec5a101b88450de
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-07-07 03:22:19 +0500

    layout changes

--- client/src/routes/profilePage/ProfilePage.jsx
+++ client/src/routes/profilePage/ProfilePage.jsx
@@ -3,19 +3,13 @@ import List from "../../components/list/List";
 import "./profilePage.scss";
 import apiRequest from "../../lib/apiRequest";
 import { useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 function ProfilePage() {
   const { currentUser, updateUser } = useContext(AuthContext);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!currentUser) {
-      navigate("/login");
-    }
-  }, [currentUser, navigate]);
 
   const handleLogout = async () => {
     try {
@@ -27,45 +21,43 @@ function ProfilePage() {
     }
   };
   return (
-    currentUser && (
-      <div className="profilePage">
-        <div className="details">
-          <div className="wrapper">
-            <div className="title">
-              <h1>User Information</h1>
-              <button>Update Profile</button>
-            </div>
-            <div className="info">
-              <span>
-                Avatar:
-                <img src={currentUser.avatar || "/logo.png"} alt="" />
-              </span>
-              <span>
-                Username: <b>{currentUser.username}</b>
-              </span>
-              <span>
-                E-mail: <b>{currentUser.email}</b>
-              </span>
-              <button onClick={handleLogout}>Logout</button>
-            </div>
-            <div className="title">
-              <h1>My List</h1>
-              <button>Create New Post</button>
-            </div>
-            <List />
-            <div className="title">
-              <h1>Saved List</h1>
-            </div>
-            <List />
+    <div className="profilePage">
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
           </div>
-        </div>
-        <div className="chatContainer">
-          <div className="wrapper">
-            <Chat />
+          <div className="info">
+            <span>
+              Avatar:
+              <img src={currentUser.avatar || "/logo.png"} alt="" />
+            </span>
+            <span>
+              Username: <b>{currentUser.username}</b>
+            </span>
+            <span>
+              E-mail: <b>{currentUser.email}</b>
+            </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
         </div>
       </div>
-    )
+      <div className="chatContainer">
+        <div className="wrapper">
+          <Chat />
+        </div>
+      </div>
+    </div>
   );
 }
 
