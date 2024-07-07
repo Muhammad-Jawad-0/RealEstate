3e8689c5d5e36d392ad3f1eab52b8970a09d6004
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-07-07 19:09:48 +0500

    comments remove

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,82 +1,29 @@
 import prisma from "../lib/prisma.js";
 import bcrypt from "bcrypt";
 
-// export const getUsers = async (req, res) => {
-//     try {
-//         const users = await prisma.user.findMany();
-//         res.status(200).json({ data: users });
-//     } catch (error) {
-//         console.log("error", error);
-//         res.status(500).json({ status: 500, message: "Failed To get Users" });
-//     }
-// };
-// // ------------------------------------------------------------
-// export const getUser = async (req, res) => {
-//     const id = req.params.id;
-//     try {
-//         const user = await prisma.user.findUnique({
-//             where: { id },
-//         });
-//         res.status(200).json({ data: user });
-//     } catch (error) {
-//         console.log("error", error);
-//         res.status(500).json({ status: 500, message: "Failed To get User" });
-//     }
-// };
-// // --------------------------------------------------------------
-// export const updateUser = async (req, res) => {
-//     const id = req.params.id;
-//     const tokenUserId = req.userId;
-//     const body = req.body;
-
-//     if (id !== tokenUserId) {
-//         return res.status(403).json({ status: 403, message: "Not Authurized" })
-//     }
-//     try {
-//         const updatedUser = await prisma.user.update({
-//             where: { id },
-//             data: body,
-//         });
-//         // res.status(200).json({ status: 200, data: updatedUser })
-//         res.status(200).json(updatedUser)
-//     } catch (error) {
-//         console.log("error", error);
-//         res.status(500).json({ status: 500, message: "Failed To update Users" });
-//     }
-// };
-// // ===============================================================================
-// export const deleteUser = async (req, res) => {
-//     try {
-//     } catch (error) {
-//         console.log("error", error);
-//         res.status(500).json({ status: 500, message: "Failed To delete Users" });
-//     }
-// };
-
 export const getUsers = async (req, res) => {
-  console.log("its work");
   try {
     const users = await prisma.user.findMany();
-    res.status(200).json(users);
+    res.status(200).json({ data: users });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed To get Users!" });
+    console.log("error", error);
+    res.status(500).json({ status: 500, message: "Failed To get Users" });
   }
 };
-// -------------
+// ------------------------------------------------------------
 export const getUser = async (req, res) => {
   const id = req.params.id;
   try {
     const user = await prisma.user.findUnique({
       where: { id },
     });
-    res.status(200).json(user);
+    res.status(200).json({ data: user });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed To get User!" });
+    console.log("error", error);
+    res.status(500).json({ status: 500, message: "Failed To get User" });
   }
 };
-// -------------
+// --------------------------------------------------------------
 export const updateUser = async (req, res) => {
   const id = req.params.id;
   const tokenUserId = req.userId;
@@ -100,14 +47,13 @@ export const updateUser = async (req, res) => {
         ...(avatar && { avatar }),
       },
     });
-    // res.status(200).json({ status: 200, data: updatedUser })
-    res.status(200).json(updatedUser);
+    res.status(200).json({ status: 200, data: updatedUser })
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed To update User!" });
+    console.log("error", error);
+    res.status(500).json({ status: 500, message: "Failed To update Users" });
   }
 };
-// -------------
+// ===============================================================================
 export const deleteUser = async (req, res) => {
   const id = req.params.id;
   const tokenUserId = req.userId;
@@ -120,7 +66,7 @@ export const deleteUser = async (req, res) => {
     await prisma.user.delete({
       where: { id },
     });
-    res.status(200).json({message:"User Deleted"})
+    res.status(200).json({ message: "User Deleted" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed To delete User!" });
